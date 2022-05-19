01215c4951e28a68e5bf7d873831874ba60bf25c
Dean.Sanders <73960745+ds797@users.noreply.github.com>
2022-05-19 11:44:18 -0700

    Enable routes

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+const App = () => {
+	return (
+		<p>About us</p>
+	);
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import './home.css';
+
+const App = () => {
+	return (
+		<main>
+			<p>Testing</p>
+		</main>
+	);
+}
+
+export default App;
