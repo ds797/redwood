be9bcd28b15d05e29887149265513a9390d917eb
DoubleAcrobat20 <tradinggame12345@gmail.com>
2022-05-24 10:59:17 -0700

    dropdown works now

--- src/components/BarElement.js
+++ src/components/BarElement.js
@@ -3,11 +3,16 @@ import "./directory.css"
 
 
 function BarElement(props){
+    var list = props.sideHrefs === undefined ? [] : props.sideHrefs.map(({text})=>{
+        return <button key={text} className="BarElement Rel dropdown-element">{text}</button>
+    })
+
     return (
         <>
         <div className="BarElementWrapper">
             <button className={props.rel==="#" ? "BarElement noRel" : "BarElement Rel"}>{props.text}</button>
             <div className="dropdown" id={props.rel}>
+                {list}
             </div>
         </div>
         </>    
