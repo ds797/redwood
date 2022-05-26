b8a641c1bda14e475e4f306267abd03bfbc4b689
DoubleAcrobat20 <tradinggame12345@gmail.com>
2022-05-26 10:36:46 -0700

    only access semester 2 units from semester 2

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,8 +4,8 @@ import DirectBar from "./DirectBar.js"
 import {useState} from 'react'
 
 var directoryList = [
-	{rel: "#sem1", text: "Semester 1", sideHrefs: [{rel : "#", text : "Unit 1 - Trig"}]},
-	{rel: "#sem2", text: "Semester 2", sideHrefs: [{rel : "#", text : "Unit 2 - Triangles"}]}
+	{rel: "#sem1", text: "Semester 1", sideHrefs: []},
+	{rel: "#sem2", text: "Semester 2", sideHrefs: [{rel : "#", text : "Unit 1 - Trig Identities"}]}
 ]
 
 export const Page = ({baseContent = <><h1>No page found!</h1><p>Try navigating to another page.</p></>}) => {
