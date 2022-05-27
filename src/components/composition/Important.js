a710d9d3e9267ebf9ef5561effd1739d15670388
DoubleAcrobat20 <tradinggame12345@gmail.com>
2022-05-27 10:12:11 -0700

    Near finished on page1

--- src/components/composition/Important.js
+++ src/components/composition/Important.js
@@ -1,6 +1,6 @@
 const Important = ({ title, content }) => {
 	return (
-		<main style={{ width: 'fit-content', padding: '1rem', border: '0.125rem solid black', borderRadius: '0.5rem' }}>
+		<main style={{maxHeight: 'fit-content',maxWidth: 'fit-content', padding: '1rem', border: '0.125rem solid black', borderRadius: '0.5rem' , alignSelf: 'flex-start'}}>
 			<h3>{title}</h3>
 			<div style={{ display: 'flex', flexFlow: 'column' }}>
 				{content}
